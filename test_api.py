32aae180cb46a37ff48bb44fd05b2227f3b47b30
Hemant <hemantgandhi259@gmail.com>
2026-09-18 15:54:45 +0530

    adding some comnmments

--- test_api.py
+++ test_api.py
@@ -12,7 +12,7 @@ questions = [
 
 for q in questions:
     response = requests.post(
-        "http://localhost:8000/chat",
+        "http://localhost:8000/chat", #dgdfgdfgfdg
         json={"question": q}
     )
 
